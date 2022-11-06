154744c5fa44a831ee4303eba5695dacc14b9a2e
Doouble1337 <katulevskiy@gmail.com>
2022-11-05 19:43:51 -0700

    NOT WORKING

--- src/main/java/letovo/server/warn-system/CommandHandler.java
+++ src/main/java/letovo/server/warn-system/CommandHandler.java
@@ -1,5 +1,13 @@
-
-
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 public class CommandHandler implements CommandExecutor {
     
     @Override
@@ -8,6 +16,54 @@ public class CommandHandler implements CommandExecutor {
         if (cmd.getName().equalsIgnoreCase("warn")){
             LOGGER.info("warn command issued by " + sender);
             String nickname = args[0];
+
+            // Read list of warned players from file
+            ArrayList<ArrayList<String>> filedata = new ArrayList<ArrayList<String>>();
+            try {
+                File myObj = new File("warns.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    filedata.add(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            
+            ArrayList <String> users = filedata[0];
+            ArrayList <String> warns = filedata[1];
+
+            int userwarns = 0;
+
+            if(users.contais(nickname)){
+                warns[users.indexOf(nickname)] += 1;
+                userwarns = warns[users.indexOf(nickname)];
+            }
+
+            else{
+                users.add(nickname);
+                warns.add(1);
+                userwarns += 1;
+                // Add a warn to person
+                try {
+                    FileWriter myWriter = new FileWriter("warns.txt");
+                    myWriter.write(users);
+                    myWriter.write(warns);
+                    myWriter.close();
+                } catch (IOException e) {
+                    LOGGER.info("An error occurred while adding a person to warned list.");
+                    e.printStackTrace();
+                }
+            }
+
+            if (userwarns >= 5){
+                ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+                String command = "ban " + nickname + " [LETOVO ANTICHEAT] SKILL ISSUE";
+                Bukkit.dispatchCommand(console, command);
+            }
+
             return true;
         }
 
--- src/main/java/letovo/server/warn-system/Plugin.java
+++ src/main/java/letovo/server/warn-system/Plugin.java
@@ -2,10 +2,12 @@ package letovo.server;
 
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import CommandHandler;
 
-/*
- * warn-system java plugin
- */
 public class Plugin extends JavaPlugin
 {
   private CommandHandler commandHandler;
@@ -14,6 +16,21 @@ public class Plugin extends JavaPlugin
 
   public void onEnable()
   {
+
+    // Create warns database if does not exist on the server startup
+    try {
+      File myObj = new File("warns.txt");
+      if (myObj.createNewFile()) {
+        LOGGER.info("File created: " + myObj.getName());
+      } else {
+        LOGGER.info("File already exists.");
+      }
+    } catch (IOException e) {
+      LOGGER.info("An error occurred in file creation (warns.txt).");
+      e.printStackTrace();
+    }
+
+    // Assign command handler to custom class
     LOGGER.info("warn-system enabled");
     commandHandler = new CommandHandler();
     getCommand("warn").setExecutor(commandHandler);
